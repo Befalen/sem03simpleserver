4658aad1b58733ac7233b7046c73794f9d3a35f6
Befalen <joakimnormann@yahoo.com>
2023-04-26 16:44:10 +0000

    changes

--- go.mod
+++ go.mod
@@ -1,5 +1,11 @@
-module github.com/Befalen/sem03simpleserver
+module github.com/Befalen/sem03proxyserver
 
 go 1.18
 
 require github.com/Befalen/is105sem03 v0.0.0-20230423131103-f668acc2c43a
+
+require (
+	github.com/Befalen/funtemps v0.0.0-20230426155739-05aeb745721c // indirect
+	github.com/Befalen/minyr v0.0.0-20230426155900-daaabb8822f6 // indirect
+	github.com/befalen/funtemps v0.0.0-20230224225114-ecca1d6e9565 // indirect
+)
--- tcp_server.go
+++ tcp_server.go
@@ -5,14 +5,19 @@ import (
 	"log"
 	"net"
 	"sync"
+    "strings"
 	"github.com/Befalen/is105sem03/mycrypt"
+    "github.com/Befalen/funtemps/conv"
+    "errors"
+    "fmt"
+    "strconv"
 )
 
 func main() {
 
 	var wg sync.WaitGroup
 
-	server, err := net.Listen("tcp", "172.17.0.2:8080")
+	server, err := net.Listen("tcp", "172.17.0.3:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -36,25 +41,27 @@ func main() {
 							log.Println(err)
 						}
 						return // fra for løkke
-					}
-					switch msg := string(buf[:n]); msg {
-					case "ping":
-						_, err = c.Write([]byte("pong"))
-					default:
-						_, err = c.Write(buf[:n])
-					}
-					if err != nil {
-						if err != io.EOF {
-							log.Println(err)
-						}
-						return // fra for løkke
-					}
+					} 
+                  
 					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
 					log.Println("Dekrypter melding: ", string(dekryptertMelding))
-					switch msg := string(dekryptertMelding); msg {
-					// Handle the decrypted message here
+					switch string(dekryptertMelding) {
+  				        case "ping":
+						kryptertMelding := mycrypt.Krypter([]rune("pong"), mycrypt.ALF_SEM03, 4)
+						log.Println("Kryptert melding: ", string(kryptertMelding))
+						_, err = c.Write([]byte(string(kryptertMelding)))
+					default:
+						if strings.HasPrefix(string(dekryptertMelding), "Kjevik") {
+							fahrenheitLine, _ := CelsiusToFarhenheit(string(dekryptertMelding))
+							kryptertMelding := mycrypt.Krypter([]rune(fahrenheitLine), mycrypt.ALF_SEM03, 4)
+							_, err = c.Write([]byte(string(kryptertMelding)))
+							log.Println("Kryptert melding: ", string(kryptertMelding))
+						} else {
+							kryptertMelding := mycrypt.Krypter([]rune(dekryptertMelding), mycrypt.ALF_SEM03, 4)
+							log.Println("Kryptert melding: ", string(kryptertMelding))
+							_, err = c.Write([]byte(string(kryptertMelding)))
+						}
 					}
-					_, err = c.Write([]byte(string(dekryptertMelding)))
 					if err != nil {
 						if err != io.EOF {
 							log.Println(err)
@@ -66,4 +73,37 @@ func main() {
 		}
 	}()
 	wg.Wait()
+}
+
+
+func CelsiusToFahrenheitString(celsius string) (string, error) {
+	var fahrFloat float64
+	var err error
+	if celsiusFloat, err := strconv.ParseFloat(celsius, 64); err == nil {
+		fahrFloat = conv.CelsiusToFahrenheit(celsiusFloat)
+	}
+	fahrString := fmt.Sprintf("%.1f", fahrFloat)
+	return fahrString, err
+}
+
+func CelsiusToFarhenheit(line string) (string, error) {
+	dividedString := strings.Split(line, ";")
+	var err error
+
+	if len(dividedString) == 4 {
+		if dividedString[3] != "" {
+
+			dividedString[3], err = CelsiusToFahrenheitString(dividedString[3])
+			if err != nil {
+				return "", err
+			}
+		} else {
+			return "Data er basert paa gyldig data (per 18.03.2023) (CC BY 4.0) fra Meteorologisk institutt (MET);endringen er gjort av Joakim Normann;;", nil
+		}
+	} else {
+		return "", errors.New("linje har ikke forventet format")
+	}
+	return strings.Join(dividedString, ";"), nil
+
+
 }
