8364d595afa15ee24935f33bc7f1ec91489d0754
Befalen <joakimnormann@yahoo.com>
2023-04-26 18:02:21 +0000

    endelig endring

--- tcp_server.go
+++ tcp_server.go
@@ -7,103 +7,87 @@ import (
 	"sync"
     "strings"
 	"github.com/Befalen/is105sem03/mycrypt"
-    "github.com/Befalen/funtemps/conv"
-    "errors"
-    "fmt"
-    "strconv"
+    "github.com/Befalen/minyr/yr"
 )
 
 func main() {
-
 	var wg sync.WaitGroup
 
 	server, err := net.Listen("tcp", "172.17.0.3:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer server.Close()
+
 	log.Printf("bundet til %s", server.Addr().String())
+
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
+
 		for {
 			log.Println("før server.Accept() kallet")
+
 			conn, err := server.Accept()
 			if err != nil {
-				return
+				log.Println(err)
+				continue
 			}
+
+			wg.Add(1)
 			go func(c net.Conn) {
-				defer c.Close()
+				defer wg.Done()
+				defer conn.Close()
+
 				for {
-					buf := make([]byte, 1024)
+					buf := make([]byte, 2048)
 					n, err := c.Read(buf)
 					if err != nil {
 						if err != io.EOF {
 							log.Println(err)
 						}
-						return // fra for løkke
-					} 
-                  
-					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
-					log.Println("Dekrypter melding: ", string(dekryptertMelding))
-					switch string(dekryptertMelding) {
-  				        case "ping":
-						kryptertMelding := mycrypt.Krypter([]rune("pong"), mycrypt.ALF_SEM03, 4)
-						log.Println("Kryptert melding: ", string(kryptertMelding))
-						_, err = c.Write([]byte(string(kryptertMelding)))
+						return
+					}
+
+					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4) //Dekryptere meldingen
+					log.Println("Dekrypter melding: ", string(dekryptertMelding))                                              //Printer den dekrypte meldingen
+
+					msgString := string(dekryptertMelding) //Meldingen konverteres til string og lagres i msgString
+
+					switch msgString {
+					case "ping":
+						kryptertMelding := mycrypt.Krypter([]rune("pong"), mycrypt.ALF_SEM03, -4) //Dette krypterer til pong
+						log.Println("Kryptert melding: ", string(kryptertMelding))                //Printer den krypterte meldingen
+						_, err = c.Write([]byte(string(kryptertMelding)))                         //Den krypterte meldingen blir sendt tilbake til klienten
+
 					default:
-						if strings.HasPrefix(string(dekryptertMelding), "Kjevik") {
-							fahrenheitLine, _ := CelsiusToFarhenheit(string(dekryptertMelding))
-							kryptertMelding := mycrypt.Krypter([]rune(fahrenheitLine), mycrypt.ALF_SEM03, 4)
-							_, err = c.Write([]byte(string(kryptertMelding)))
-							log.Println("Kryptert melding: ", string(kryptertMelding))
+						if strings.HasPrefix(msgString, "Kjevik") { //Hvis input begynner med Kjevik
+							newString, err := yr.CelsiusToFahrenheitLine("Kjevik;SN39040;18.03.2022 01:50;6") //Dette sender til yr.CelsiusToFahrenheitLine() for å få en streng som viser en temperaturkonvertering fra Celsius til Fahrenheit
+							if err != nil {
+								log.Fatal(err)
+							}
+
+							kryptertMelding := mycrypt.Krypter([]rune(newString), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4) // Dette krypterer newString
+							_, err = conn.Write([]byte(string(kryptertMelding)))                                               //Den krypterte meldingen sendes tilbake til klient
 						} else {
-							kryptertMelding := mycrypt.Krypter([]rune(dekryptertMelding), mycrypt.ALF_SEM03, 4)
-							log.Println("Kryptert melding: ", string(kryptertMelding))
-							_, err = c.Write([]byte(string(kryptertMelding)))
+							kryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4) // Dette krypterer meldingen
+							_, err = c.Write([]byte(string(kryptertMelding)))                                                        //Hvis ikke ping eller Kjevik sendes input tilbake som kryptert
+
 						}
 					}
+
 					if err != nil {
 						if err != io.EOF {
 							log.Println(err)
 						}
-						return // fra for løkke
+						return
 					}
 				}
 			}(conn)
 		}
 	}()
+
 	wg.Wait()
 }
 
-
-func CelsiusToFahrenheitString(celsius string) (string, error) {
-	var fahrFloat float64
-	var err error
-	if celsiusFloat, err := strconv.ParseFloat(celsius, 64); err == nil {
-		fahrFloat = conv.CelsiusToFahrenheit(celsiusFloat)
-	}
-	fahrString := fmt.Sprintf("%.1f", fahrFloat)
-	return fahrString, err
-}
-
-func CelsiusToFarhenheit(line string) (string, error) {
-	dividedString := strings.Split(line, ";")
-	var err error
-
-	if len(dividedString) == 4 {
-		if dividedString[3] != "" {
-
-			dividedString[3], err = CelsiusToFahrenheitString(dividedString[3])
-			if err != nil {
-				return "", err
-			}
-		} else {
-			return "Data er basert paa gyldig data (per 18.03.2023) (CC BY 4.0) fra Meteorologisk institutt (MET);endringen er gjort av Joakim Normann;;", nil
-		}
-	} else {
-		return "", errors.New("linje har ikke forventet format")
-	}
-	return strings.Join(dividedString, ";"), nil
-
-
-}
+	
